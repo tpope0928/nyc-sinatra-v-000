1a929ab68387423c45d29594c3fffbc8f3f2ae9e
Thomas Pope <tpope0928@gmail.com>
2019-03-07 14:41:05 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,4 @@
 class FiguresController < ApplicationController
-  # add controller methods
   get '/figures' do
     @figures = Figure.all
     erb :'figures/index'
@@ -20,12 +19,12 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(params[:owner])
-    if !params[:landmark][:name].empty?
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
 
-    if !params[:title][:name].empty?
+    unless params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
     end
 
@@ -36,11 +35,10 @@ class FiguresController < ApplicationController
   patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     @figure.update(params[:figure])
-
-    if !params[:title][:name].empty?
+    unless params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
     end
-    if !params[:landmark][:name].empty?
+    unless params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
     @figure.save
